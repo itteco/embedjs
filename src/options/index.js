a05c5ba8d0fe07ef0a242aa468c2564209e2421a
Nazar Leush <n.leush@gmail.com>
2019-03-22 22:32:06 +0200

    change buildOptionsForm params

--- src/options/index.js
+++ src/options/index.js
@@ -2,11 +2,11 @@ var iframely = require('../iframely');
 var formBuilder = require('./form-builder');
 var renderer = require('./renderer');
 
-iframely.buildOptionsForm = function(params) {
+iframely.buildOptionsForm = function(id, formContainer, options) {
     formBuilder({
-        id:             params.id,
-        formContainer:  params.formContainer,
-        options:        params.options,                        
+        id:             id,
+        formContainer:  formContainer,
+        options:        options,                        
         renderer:       renderer
     });
 };
