bbc92bff6eb0e500784bdd864f68daee054286b0
Nazar Leush <n.leush@gmail.com>
2021-08-06 21:17:26 +0300

    allow redefine `iframely.openHref` function

--- src/widget-click.js
+++ src/widget-click.js
@@ -6,15 +6,42 @@ iframely.on('message', function(widget, message) {
     }
 });
 
+/**
+ * Allows redefine `openHref` in following way:
+ * 
+ *  iframely.openHref = function(href) {
+ *      if (allowUsualOpen(href)) {
+ *          // Default `iframely` workflow.
+ *          return true;
+ *      }
+ *      if (needCustomOpen(href)) {
+ *          // Custom user workflow.
+ *          customOpenHref(href);
+ *      }
+ * 
+ *      // Prevent open link.
+ *      return false;   // Or any falsy value.
+ *  };
+ */
+
 iframely.on('open-href', function(href) {
 
     iframely.triggerAsync('click', href);
 
-    if (href.indexOf(window.location.origin) === 0) {
-        // Redirect top on same origin.
-        window.location.href = href;
-    } else {
-        // Open new tab on another origin.
-        window.open(href, '_blank', 'noopener');
+    var allowOpen = true;
+
+    if (typeof iframely.openHref === 'function') {
+        // Usual flow only if `true` returned.
+        allowOpen = iframely.openHref(href);
+    }
+
+    if (allowOpen) {
+        if (href.indexOf(window.location.origin) === 0) {
+            // Redirect top on same origin.
+            window.location.href = href;
+        } else {
+            // Open new tab on another origin.
+            window.open(href, '_blank', 'noopener');
+        }
     }
 });
