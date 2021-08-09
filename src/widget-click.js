6160f2bab39a4be7c9cc796f88469966fdf56d49
Nazar Leush <n.leush@gmail.com>
2021-08-09 19:01:47 +0300

    simplify usage of `iframely.openHref`

--- src/widget-click.js
+++ src/widget-click.js
@@ -6,42 +6,17 @@ iframely.on('message', function(widget, message) {
     }
 });
 
-/**
- * Allows redefine `openHref` in following way:
- * 
- *  iframely.openHref = function(href) {
- *      if (allowUsualOpen(href)) {
- *          // Default `iframely` workflow.
- *          return true;
- *      }
- *      if (needCustomOpen(href)) {
- *          // Custom user workflow.
- *          customOpenHref(href);
- *      }
- * 
- *      // Prevent open link.
- *      return false;   // Or any falsy value.
- *  };
- */
+iframely.openHref = function(href) {
+    if (href.indexOf(window.location.origin) === 0) {
+        // Redirect top on same origin.
+        window.location.href = href;
+    } else {
+        // Open new tab on another origin.
+        window.open(href, '_blank', 'noopener');
+    }
+};
 
 iframely.on('open-href', function(href) {
-
     iframely.triggerAsync('click', href);
-
-    var allowOpen = true;
-
-    if (typeof iframely.openHref === 'function') {
-        // Usual flow only if `true` returned.
-        allowOpen = iframely.openHref(href);
-    }
-
-    if (allowOpen) {
-        if (href.indexOf(window.location.origin) === 0) {
-            // Redirect top on same origin.
-            window.location.href = href;
-        } else {
-            // Open new tab on another origin.
-            window.open(href, '_blank', 'noopener');
-        }
-    }
+    iframely.openHref(href);
 });
