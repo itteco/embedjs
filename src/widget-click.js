fc54cdb1738a8519e4b8ab81a7178ca691054f83
Nazar Leush <n.leush@gmail.com>
2021-08-09 19:39:42 +0300

    `iframely.openHref` - prevent override user handler

--- src/widget-click.js
+++ src/widget-click.js
@@ -6,15 +6,18 @@ iframely.on('message', function(widget, message) {
     }
 });
 
-iframely.openHref = function(href) {
-    if (href.indexOf(window.location.origin) === 0) {
-        // Redirect top on same origin.
-        window.location.href = href;
-    } else {
-        // Open new tab on another origin.
-        window.open(href, '_blank', 'noopener');
-    }
-};
+// Do not override user defined handler.
+if (!iframely.openHref) {
+    iframely.openHref = function(href) {
+        if (href.indexOf(window.location.origin) === 0) {
+            // Redirect top on same origin.
+            window.location.href = href;
+        } else {
+            // Open new tab on another origin.
+            window.open(href, '_blank', 'noopener');
+        }
+    };
+}
 
 iframely.on('open-href', function(href) {
     iframely.triggerAsync('click', href);
