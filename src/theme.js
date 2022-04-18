3d9104602f4d60173cdd6d2e6a32540492314907
Nazar Leush <n.leush@gmail.com>
2022-04-18 22:01:29 +0300

    `setTheme` for `iframe` or container

--- src/theme.js
+++ src/theme.js
@@ -8,23 +8,30 @@ function setThemeInIframe(iframe, theme) {
     }, '*', iframe.contentWindow);
 }
 
-function setThemeInAllIframes(theme) {
-    var iframes = document.getElementsByTagName('iframe');
+function setThemeInAllIframes(parent, theme) {
+    var iframes = parent.getElementsByTagName('iframe');
     for(var i = 0; i < iframes.length; i++) {
         var iframe = iframes[i];
         setThemeInIframe(iframe, theme);
     }
 }
 
-iframely.setTheme = function(theme) {
+iframely.setTheme = function(theme, container) {
     if (theme && iframely.SUPPORTED_THEMES.indexOf(theme) > -1) {
-        // Send get param to next iframes.
-        iframely.extendOptions({
-            theme: theme
-        });
-        setThemeInAllIframes(theme);
-        iframely.trigger('set-theme', theme);
-        // TODO: set theme in future iframes? - single iframe theme api for user?
+        if (container) {
+            if (container.tagName === 'IFRAME') {
+                setThemeInIframe(container, theme);
+            } else {
+                setThemeInAllIframes(container, theme);
+            }
+        } else {
+            // Send get param to next iframes.
+            iframely.extendOptions({
+                theme: theme
+            });
+            setThemeInAllIframes(document, theme);
+            iframely.trigger('set-theme', theme);
+        }
     } else {
         console.warn('Using iframely.setTheme with not supported theme: "' + theme + '". Supported themes are: ' + iframely.SUPPORTED_THEMES.join(', '));
     }
