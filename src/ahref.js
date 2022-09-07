fe445696fcc39bc341ab9adf498211e74e55ade7
Nazar Leush <n.leush@gmail.com>
2022-09-08 00:46:56 +0300

    Revert "allow send query options to `iframely.load(.., options)`"
    This reverts commit 1e2499882861c541a96e95fd53538ea9374e3a15.

--- src/ahref.js
+++ src/ahref.js
@@ -1,36 +1,36 @@
 var utils = require('./utils');
 var iframely = require('./iframely');
 
-iframely.on('load', function(container, href, options) {
+iframely.on('load', function(container, href) {
     if (container && container.nodeName && typeof href === 'string') {
         var a = document.createElement('a');
         a.setAttribute('href', href);
         container.appendChild(a);
-        iframely.trigger('load', a, options);
+        iframely.trigger('load', a);
     }
 });
 
-iframely.on('load', function(el, options) {
+iframely.on('load', function(el) {
 
     if (!el && !iframely.import) { 
 
         var elements = document.querySelectorAll('a[data-iframely-url]:not([data-import-uri])');
         for(var i = 0; i < elements.length; i++) {
-            iframely.trigger('load', elements[i], options);
+            iframely.trigger('load', elements[i]);
         }
     }
     
 });
 
-iframely.on('load', function(el, options) {
+iframely.on('load', function(el) {
 
     if (el && el.nodeName === 'A' && (el.getAttribute('data-iframely-url') || el.getAttribute('href')) && !el.hasAttribute('data-import-uri')) {
-        unfurl(el, options);
+        unfurl(el);
     }
     
 });
 
-function unfurl(el, options) {
+function unfurl(el) {
     if (!el.getAttribute('data-iframely-url') && !el.getAttribute('href')) {
         return; // isn't valid
     }
@@ -38,18 +38,18 @@ function unfurl(el, options) {
 
     var dataIframelyUrl = el.getAttribute('data-iframely-url');
     if (dataIframelyUrl && /^((?:https?:)?\/\/[^/]+)\/\w+/i.test(dataIframelyUrl)) {
-        src = utils.getEndpoint(dataIframelyUrl, Object.assign(options || {}, {
+        src = utils.getEndpoint(dataIframelyUrl, {
             v: iframely.VERSION,
             app: 1,
             theme: iframely.config.theme
-        }));
+        });
     } else if ((iframely.config.api_key || iframely.config.key) && iframely.CDN) {
-        src = utils.getEndpoint('/api/iframe', Object.assign(options || {}, {
+        src = utils.getEndpoint('/api/iframe', {
             url: el.getAttribute('href'),
             v: iframely.VERSION,
             app: 1,
             theme: iframely.config.theme
-        }), iframely.SUPPORTED_QUERY_STRING);
+        }, iframely.SUPPORTED_QUERY_STRING);
     } else {
         console.warn('Iframely cannot build embeds: api key is required as query-string of embed.js');
     }
